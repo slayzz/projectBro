639dcb5cff1a55b056f28bbb9c35ee159376fdf5
Slayzz <slayzz@mail.ru>
2017-04-15 06:05:45 +0300

    Раздача Файла

--- src/server/HttpEvent.cxx
+++ src/server/HttpEvent.cxx
@@ -1,16 +1,18 @@
-#include <functional>
 #include <iostream>
+#include <fstream>
 #include <event2/event.h>
 #include <event2/http.h>
 #include <event2/buffer.h>
 #include <evhttp.h>
+#include <cstring>
+#include <string>
+#include <cstdlib>
 #include "HttpEvent.hxx"
 
 HttpEvent::HttpEvent(Types::SOCKET& socket) : EventBase() {
   httpWatcher_ = evhttp_new(getEventBase());
   evhttp_accept_socket(httpWatcher_, socket);
   installHandlers();
-  evhttp_set_gencb(httpWatcher_, &HttpEvent::defaultHandler, 0);
 }
 
 void HttpEvent::run() {
@@ -36,6 +38,7 @@ void HttpEvent::installHandlers() {
   for(auto handler : handlers_) {
     evhttp_set_cb(httpWatcher_, handler->getUrl().c_str(), &HttpEvent::handleRequest, handler);
   }
+  evhttp_set_gencb(httpWatcher_, &HttpEvent::defaultHandler, 0);
 }
 
 void HttpEvent::handleRequest(struct evhttp_request *req, void *arg) {
@@ -45,27 +48,38 @@ void HttpEvent::handleRequest(struct evhttp_request *req, void *arg) {
 
 void HttpEvent::defaultHandler(struct evhttp_request *req, void *arg) {
   auto evb = evbuffer_new();
-  auto uri = evhttp_request_get_uri(req);
-  auto host = evhttp_request_get_host(req);
-  std::cout << uri << std::endl;
-  std::cout << host << std::endl;
-  const char serverName[] = "Pipka";
-  evbuffer_add_printf(evb, "<HTML><HEAD><TITLE>%s Page</TITLE></HEAD><BODY>\n", serverName);
+  const size_t bufferSize = 10024;
+  const char serverName[] = "Ultra Server";
+  char buffer[bufferSize];
+
+  int position = 0;
+  std::ifstream file("../web/html/index.html");
+  while(!file.eof()) {
+    file.get(buffer[position++]);
+  }
+  buffer[position-1] = '\0';
+
+  evbuffer_add(evb, buffer, strlen(buffer));
+
+//  evbuffer_add_printf(evb, "<HTML><HEAD><TITLE>%s Page</TITLE></HEAD><BODY>\n", serverName);
 // Add buffer
-  evbuffer_add(evb, "<div>Lol</div>", 13);
+//  evbuffer_add(evb, "<div>Lol</div>", 13);
 // Add formatted text
-  evbuffer_add_printf(evb, "Your request is <B>%s</B> from <B>%s</B>.<BR/>Your user agent is '%s'\n",
-                      req->uri, req->remote_host, evhttp_find_header(req->input_headers, "User-Agent"));
+//  evbuffer_add_printf(evb, "Your request is <B>%s</B> from <B>%s</B>.<BR/>Your user agent is '%s'\n",
+//                      req->uri, req->remote_host, evhttp_find_header(req->input_headers, "User-Agent"));
 // Add footer
-  evbuffer_add_printf(evb, "</BODY></HTML>");
+//  evbuffer_add_printf(evb, "</BODY></HTML>");
 
 // Set HTTP headers
   evhttp_add_header(req->output_headers, "Server", serverName);
   evhttp_add_header(req->output_headers, "Connection", "close");
+//  evhttp_add_header(req->output_headers, "Referrer-Policy", "no-referer");
+//  evhttp_remove_header(req->output_headers, "Referer");
 
 // Send reply
   evhttp_send_reply(req, HTTP_OK, "OK", evb);
 
+  file.close();
 // Free memory
   evbuffer_free(evb);
 }
