1416c271f6982666f16640cd7718c7c167a5fe79
Slayzz <slayzz@mail.ru>
2017-04-15 05:05:16 +0300

    Рабочий вебсокет, имеющий возмножность подписки и отписки, и приема сообщений

--- src/globals.hxx
+++ src/globals.hxx
@@ -2,8 +2,20 @@
 #define WEBSOCKET_GLOBALS_HXX
 #include <event2/event.h>
 
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+#include <set>
+using websocketpp::connection_hdl;
+
+
 namespace Types {
   typedef evutil_socket_t SOCKET;
+
+  // Websocket TypeDef
+  typedef websocketpp::server<websocketpp::config::asio> WebsocketBaseServer;
+  typedef WebsocketBaseServer::message_ptr MessagePtr;
+  typedef std::set<connection_hdl,std::owner_less<connection_hdl> >
+      ConnectionList;
 }
 
 #endif //WEBSOCKET_GLOBALS_HXX
---
+++ src/server/Server.cxx
@@ -0,0 +1,100 @@
+#include <sstream>
+
+#include <event2/event.h>
+#include <event2/bufferevent.h>
+
+#include "Server.hxx"
+
+const unsigned int Server::MAX_CONNECTS = 5;
+
+Server::Server(uint16_t port) {
+  int retCode;
+  struct sockaddr_in serverAddress;
+
+  retCode = sockInit() != 0;
+  if (retCode) {
+    std::cout << "Sock Init error" << std::endl;
+    exit(retCode);
+  }
+
+  socketFd_ = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketFd_ < 0) {
+    std::cout << "Sock Create error" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  evutil_make_socket_nonblocking(socketFd_);
+  httpEvent_ = new HttpEvent(socketFd_);
+
+#ifndef _WIN32
+  int enable = 1;
+  setsockopt(socketFd_, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+#endif
+
+  memset(&serverAddress, 0, sizeof(sockaddr_in));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(port);
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(socketFd_, (sockaddr*)&serverAddress, sizeof(serverAddress))) {
+    std::cout << "Sock Bind error" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+void Server::run() {
+  listen(socketFd_, Server::MAX_CONNECTS);
+  httpEvent_->run();
+}
+
+
+int Server::sockInit() {
+#ifdef _WIN32
+  WSADATA wsa_data;
+  return WSAStartup(MAKEWORD(1, 1), &wsa_data);
+#else
+  return 0;
+#endif
+}
+
+int Server::sockClose() {
+#ifdef _WIN32
+  return WSACleanup();
+#else
+  return 0;
+#endif
+}
+
+int Server::sockShutdown() {
+  int status = 0;
+
+#ifdef _WIN32
+  status = shutdown(sock, SD_BOTH);
+  if (status == 0) {
+    status = closesocket(sock);
+  }
+#else
+//    status = shutdown(socketFd, SHUT_RDWR);
+//    if (status == 0) { status = close(socketFd); }
+#endif
+
+  return status;
+}
+
+void Server::clearSocket() {
+  int retCode;
+  retCode = sockShutdown() != 0;
+  if (retCode) {
+    std::cout << "Sock Shutdown error" << std::endl;
+    exit(retCode);
+  }
+  retCode = sockClose() != 0;
+  if (retCode) {
+    std::cout << "Sock Close error" << std::endl;
+    exit(retCode);
+  }
+  close(socketFd_);
+}
+
+Server::~Server() {
+  clearSocket();
+}
